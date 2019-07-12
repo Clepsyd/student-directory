ee9aeccdd01010cb97b1d9178562fdcbf3e690b1
Guillaume <liard.guillaume@gmail.com>
2019-07-12 13:42:51 +0100

    can now load a list of students from students.csv

--- directory.rb
+++ directory.rb
@@ -9,18 +9,6 @@
   :hobbies
 ]
 
-def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = []
-    @students_data_fields.each { |field| student_data << student[field] }
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-  puts "List of students saved in students.csv"
-end
-
 def print_header
   puts "The students of Villains Academy"
   puts "----------------"
@@ -30,7 +18,7 @@ def print_students_list()
   count = 0
   while count < @students.length
     student = @students[count]
-      puts "#{count + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
+    puts "#{count + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
     count += 1
   end
   
@@ -42,6 +30,7 @@ def print_footer()
   else
     puts "Overall, we have #{@students.count} great student"
   end
+  puts ""
 end
 
 def input_menu
@@ -49,19 +38,27 @@ def input_menu
   continue = gets.chomp
   while !["y", "n"].include?(continue)
     puts "Please answer 'y' or 'n'. Add a student to the list? (y/n)"
-    continue = gets.chomp
+    continue = gets.strip
+    puts ""
   end
   return continue
+end
+
+def add_field(student)
+  for element in @students_data_fields
+    puts "Please enter the #{element.to_s} of the student"
+    input = gets.strip
+    puts ""
+    if input != ""
+      student[element] = element == :cohort ? input.downcase.to_sym : input
+    end
+  end
 end
 
 def input_fields
   student = Hash.new("N/A")
   # Ask the user to fill information for every data_field
-  for element in @students_data_fields
-    puts "Please enter the #{element.to_s} of the student"
-    input = gets.chomp
-    student[element] = input if input != ""  
-  end
+  add_field(student)
   @students << student
 end
 
@@ -76,6 +73,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -93,6 +91,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -100,10 +100,40 @@ def process(selection)
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = []
+    @students_data_fields.each { |field| student_data << student[field] }
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+  puts "List of students saved in students.csv"
+  puts ""
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    student = Hash.new
+    line.chomp.split(',').each_with_index { |val, index|
+      field = @students_data_fields[index]
+      student[field] = field == :cohort ? val.to_sym : val
+    }
+    @students << student
+  end
+  file.close
+  puts "File loaded"
+  puts ""
+end
+
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    input = gets.chomp
+    puts ""
+    process(input)
   end
 end
 
@@ -111,17 +141,17 @@ end
 
 ## let's put all students into an array
 # students = [
-# {name: "Dr. Hannibal Lecter", cohort: :november},
-# {name: "Darth Vader", cohort: :november},
-# {name: "Nurse Ratched", cohort: :november},
-# {name: "Michael Corleone", cohort: :november},
-# {name: "Alex DeLarge", cohort: :november},
-# {name: "The Wicked Witch of the West", cohort: :november},
-# {name: "Terminator", cohort: :november},
-# {name: "Freddy Krueger", cohort: :november},
-# {name: "The Joker", cohort: :november},
-# {name: "Joffrey Baratheon", cohort: :november},
-# {name: "Norman Bates", cohort: :november}
-# ]
-
+  # {name: "Dr. Hannibal Lecter", cohort: :november},
+  # {name: "Darth Vader", cohort: :november},
+  # {name: "Nurse Ratched", cohort: :november},
+  # {name: "Michael Corleone", cohort: :november},
+  # {name: "Alex DeLarge", cohort: :november},
+  # {name: "The Wicked Witch of the West", cohort: :november},
+  # {name: "Terminator", cohort: :november},
+  # {name: "Freddy Krueger", cohort: :november},
+  # {name: "The Joker", cohort: :november},
+  # {name: "Joffrey Baratheon", cohort: :november},
+  # {name: "Norman Bates", cohort: :november}
+  # ]
+  
 interactive_menu
