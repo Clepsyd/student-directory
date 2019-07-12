c5f33544f494260444a2bc45bead1f8296ed10aa
Guillaume <liard.guillaume@gmail.com>
2019-07-12 19:59:26 +0100

    using csv library to load files

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 @students = []
 
 # Add elements to this list if more informations are required
@@ -72,8 +74,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list to a CSV file"
+  puts "4. Load the list from a CSV file"
   puts "9. Exit"
 end
 
@@ -83,25 +85,9 @@ def show_students
   print_footer
 end
 
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
-  else
-    puts "I don't know what you meant, try again"
-  end
-end
 
 def save_students
-  file = File.open("students.csv", "w")
+  file = ask_save_file
   @students.each do |student|
     student_data = []
     @students_data_fields.each { |field| student_data << student[field] }
@@ -109,21 +95,32 @@ def save_students
     file.puts csv_line
   end
   file.close
-  puts "List of students saved in students.csv"
+  puts "List of students saved in #{filename}"
   puts ""
 end
 
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    student = Hash.new
-    line.chomp.split(',').each_with_index { |val, index|
-      field = @students_data_fields[index]
-      student[field] = field == :cohort ? val.to_sym : val
-    }
-    @students << student
+def ask_save_file
+  puts "Type the name of the file you would like to save to (.csv):"
+  loop do
+    filename = STDIN.gets.strip
+    if filename.end_with? ".csv"
+      file = File.open(filename, "w")
+      break
+    else
+     puts "Sorry but a .csv file is require, pleas try again:"
+    end
   end
-  file.close
+  return file
+end
+
+def load_students(filename = "students.csv")
+  @students.clear
+  CSV.foreach(filename) { |line|
+    student = Hash[@students_data_fields.zip line]
+    @students << student
+  }
+  puts "File Loaded"
+  puts ""
 end
 
 def try_load_students
@@ -146,6 +143,37 @@ def interactive_menu
     process(input)
   end
 end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    menu_load
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def menu_load
+  puts "Please enter the name of the file you want to load:"
+  loop do
+    file = STDIN.gets.strip
+    if File.exists?(file)
+      load_students(file)
+      break
+    else
+      puts "This file doesn't exist, please try again:"
+    end
+  end
+end
+
 
 #students = input_students
 
